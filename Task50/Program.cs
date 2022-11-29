95e5d70e6252742de0e472762e3ad9ac01fefc1b
Dima <Semonovdis10290@icloud.com>
2022-11-30 00:10:55 +0300

    DZ

---
+++ Task50/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Now you have to enter row and column");
+Console.Write("Enter the row: ");
+int Position1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Enter column: ");
+int Position2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int a = 5;
+int b = 7;
+Random random = new Random();
+int[,] array = new int[a, b];
+Console.WriteLine("Randomly generated array: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.Next(10, 99);
+        Console.Write("{0}  ", array[i, j]);
+    }
+    Console.WriteLine();
+}
+if (Position1 < 0 | Position1 > array.GetLength(0) - 1 | Position2 < 0 | Position2 > array.GetLength(1) - 1)
+{
+    Console.WriteLine("Element does not exist  ");
+}
+else
+{
+    Console.WriteLine("Element by given parameters = {0}", array[Position1, Position2]);
+}
+Console.WriteLine();
---
+++ Task52/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Enter the number of row: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of column: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+
+Console.WriteLine("\n");
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine("<- Arithmetic mean of each array column");
+Console.WriteLine();
+PrintArray(numbers);
+
